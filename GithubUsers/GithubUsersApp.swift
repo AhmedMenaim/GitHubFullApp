b061a67c52611906a5cde98afd14b7bb7304b214
CryptoOo <ahmedmenaim8@gmail.com>
2023-06-25 23:19:36 +0300

    Initial Commit

---
+++ GithubUsers/GithubUsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubUsersApp.swift
+//  GithubUsers
+//
+//  Created by Menaim on 25/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct GithubUsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
